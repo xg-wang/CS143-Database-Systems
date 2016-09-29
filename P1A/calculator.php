bcadb7362092d5625c104ef85c99e36fd9f20a0c
Xingan Wang <wangxgwxg@gmail.com>
2016-09-28 19:43:48 -0700

    php template

--- P1A/calculator.php
+++ P1A/calculator.php
@@ -1,6 +1,6 @@
 <html>
 <head>
-    <title>Calculator</title>
+  <title>Calculator</title>
 </head>
 <body>
 
@@ -8,17 +8,17 @@
 (Ver 0.1 Sep/28/2016 by Yiming Wang & Xingan Wang)<br />
 Type an expression in the following box (e.g., 10.5+20*3/25).
 <p>
-    <form method="GET">
-        <input type="text" name="expr">
-        <input type="submit" value="Calculate">
-    </form>
+  <form method="GET">
+    <input type="text" name="expr">
+    <input type="submit" value="Calculate">
+  </form>
 </p>
 
 <ul>
-    <li>Only numbers and +,-,* and / operators are allowed in the expression.
-    <li>The evaluation follows the standard operator precedence.
-    <li>The calculator does not support parentheses.
-    <li>The calculator handles invalid input "gracefully". It does not output PHP error messages.
+  <li>Only numbers and +,-,* and / operators are allowed in the expression.
+  <li>The evaluation follows the standard operator precedence.
+  <li>The calculator does not support parentheses.
+  <li>The calculator handles invalid input "gracefully". It does not output PHP error messages.
 </ul>
 
 Here are some(but not limit to) reasonable test cases:
@@ -27,3 +27,8 @@ Here are some(but not limit to) reasonable test cases:
   <li> An expression with floating point or negative sign : -3.2+2*4-1/3 = 4.46666666667, 3*-2.1*2 = -12.6 </li>
   <li> Some typos inside operation (e.g. alphabetic letter): Invalid input expression 2d4+1 </li>
 </ol>
+
+
+<?php
+  echo "<h2>Result</h2>";
+?>
