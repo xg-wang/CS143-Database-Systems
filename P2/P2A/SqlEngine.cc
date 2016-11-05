db5a580d14b6617f022538562424f86ad637479b
Xingan Wang <wangxgwxg@gmail.com>
2016-11-04 20:09:57 -0700

    indentation 2 spaces

--- P2/P2A/SqlEngine.cc
+++ P2/P2A/SqlEngine.cc
@@ -66,33 +66,33 @@ RC SqlEngine::select(int attr, const string& table, const vector<SelCond>& cond)
       // compute the difference between the tuple value and the condition value
       switch (cond[i].attr) {
       case 1:
-	diff = key - atoi(cond[i].value);
-	break;
+    diff = key - atoi(cond[i].value);
+    break;
       case 2:
-	diff = strcmp(value.c_str(), cond[i].value);
-	break;
+    diff = strcmp(value.c_str(), cond[i].value);
+    break;
       }
 
       // skip the tuple if any condition is not met
       switch (cond[i].comp) {
       case SelCond::EQ:
-	if (diff != 0) goto next_tuple;
-	break;
+    if (diff != 0) goto next_tuple;
+    break;
       case SelCond::NE:
-	if (diff == 0) goto next_tuple;
-	break;
+    if (diff == 0) goto next_tuple;
+    break;
       case SelCond::GT:
-	if (diff <= 0) goto next_tuple;
-	break;
+    if (diff <= 0) goto next_tuple;
+    break;
       case SelCond::LT:
-	if (diff >= 0) goto next_tuple;
-	break;
+    if (diff >= 0) goto next_tuple;
+    break;
       case SelCond::GE:
-	if (diff < 0) goto next_tuple;
-	break;
+    if (diff < 0) goto next_tuple;
+    break;
       case SelCond::LE:
-	if (diff > 0) goto next_tuple;
-	break;
+    if (diff > 0) goto next_tuple;
+    break;
       }
     }
 
@@ -139,41 +139,41 @@ RC SqlEngine::load(const string& table, const string& loadfile, bool index)
 
 RC SqlEngine::parseLoadLine(const string& line, int& key, string& value)
 {
-    const char *s;
-    char        c;
-    string::size_type loc;
-    
-    // ignore beginning white spaces
-    c = *(s = line.c_str());
-    while (c == ' ' || c == '\t') { c = *++s; }
+  const char *s;
+  char        c;
+  string::size_type loc;
+  
+  // ignore beginning white spaces
+  c = *(s = line.c_str());
+  while (c == ' ' || c == '\t') { c = *++s; }
 
-    // get the integer key value
-    key = atoi(s);
+  // get the integer key value
+  key = atoi(s);
 
-    // look for comma
-    s = strchr(s, ',');
-    if (s == NULL) { return RC_INVALID_FILE_FORMAT; }
+  // look for comma
+  s = strchr(s, ',');
+  if (s == NULL) { return RC_INVALID_FILE_FORMAT; }
 
-    // ignore white spaces
-    do { c = *++s; } while (c == ' ' || c == '\t');
-    
-    // if there is nothing left, set the value to empty string
-    if (c == 0) { 
-        value.erase();
-        return 0;
-    }
-
-    // is the value field delimited by ' or "?
-    if (c == '\'' || c == '"') {
-        s++;
-    } else {
-        c = '\n';
-    }
-
-    // get the value string
-    value.assign(s);
-    loc = value.find(c, 0);
-    if (loc != string::npos) { value.erase(loc); }
-
+  // ignore white spaces
+  do { c = *++s; } while (c == ' ' || c == '\t');
+  
+  // if there is nothing left, set the value to empty string
+  if (c == 0) { 
+    value.erase();
     return 0;
+  }
+
+  // is the value field delimited by ' or "?
+  if (c == '\'' || c == '"') {
+    s++;
+  } else {
+    c = '\n';
+  }
+
+  // get the value string
+  value.assign(s);
+  loc = value.find(c, 0);
+  if (loc != string::npos) { value.erase(loc); }
+
+  return 0;
 }
